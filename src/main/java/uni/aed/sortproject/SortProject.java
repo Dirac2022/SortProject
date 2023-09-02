c10edd1f3f120c6faef48e7ab1cf0bb17c982dbd
Mitchel <daronmitchel@gmail.com>
2023-09-02 12:34:03 -0500

    first commit

---
+++ src/main/java/uni/aed/sortproject/SortProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.sortproject;
+
+/**
+ *
+ * @author mitch
+ */
+
+import uni.aed.gui.SortJFrame;
+
+public class SortProject {
+
+    public static void main(String[] args) {
+        
+        SortJFrame v1 = new SortJFrame();
+        v1.setVisible(true);
+        
+    }
+}
